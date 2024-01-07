ca9dd6d7c9b4ee5556eb865316d682211af27c7f
German <germanoctako@gmail.com>
2024-01-07 03:03:49 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+setuptools.setup(
+    name="pip_test",
+    version="0.1.0",
+    author="Antonio German Marquez Trujillo",
+    author_email="germanoctako@gmail.com",
+    description="Prove setup.py files",
+    python_requires='>=3.9',
+    install_requires=[
+        "urllib3>=1.21.1,<1.27",
+        "flask>1.0,<2.0",
+        "Werkzeug>=2.2.2"
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-mock',
+            'prospector',
+            'mypy',
+            'coverage',
+        ]
+    }
+)
