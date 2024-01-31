d14d52d480e84b147dcdf234cda17a86adae7339
German <germanoctako@gmail.com>
2024-01-31 17:04:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="Prove setup.py files",
     python_requires='>=3.9',
     install_requires=[
-        "urllib3>=1.21.1,<1.27",
+        "urllib3>=1.21.5,<1.27",
         "flask>1.0,<2.0",
         "Werkzeug>=2.2.2"
     ],
